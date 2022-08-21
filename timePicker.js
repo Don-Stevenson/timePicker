28fe261c8861e8b47775db58db7e471f0d143c19
Don Stevenson <dsteven1@gmail.com>
2022-08-21 14:38:25 -0400

    Add working min and hour generator

--- timePicker.js
+++ timePicker.js
@@ -2,11 +2,6 @@ const prompts = require("prompts");
 
 // helpers
 // **********
-const validateNum = (value) => {
-  if (value > 12 || value < 1 || isNaN(parseFloat(value))) {
-    return "Error!! Please enter a whole number between 0 and 12";
-  } else return true;
-};
 
 const validateAmOrPm = (value) => {
   if (value === "am" || value === "pm") {
@@ -14,38 +9,37 @@ const validateAmOrPm = (value) => {
   } else return `Error!! Please enter either "am" or "pm" only`;
 };
 
+const hour = (Math.random() * 12).toFixed(0);
+
+const minGen = () => {
+  const mins = Math.round((Math.random() * 60) / 15) * 15;
+  return mins === 60 || mins === 0 ? "00" : mins;
+};
+
 
 const questions = [
-  {
-    type: "number",
-    name: "hourRange",
-    initial: `number between 1 and 12`,
-    message: `
-* Welcome to TimePickr! *
-**************************
-A program to help you pick a time`,
-    validate: (hourRange) => validateNum(hourRange),
-  },
   {
     type: "text",
     name: "amOrPm",
     initial: `enter "am" or "pm"`,
-    message: `Choose morning (am) or evening (pm)?`,
+    message: `
+* Welcome to TimePickr! *
+**************************
+A program to help you pick a time
+Choose morning (am) or evening (pm)?`,
     validate: (amOrPm) => validateAmOrPm(amOrPm),
   },
 ];
 
 (async () => {
   const response = await prompts(questions);
-  const hour = (Math.random() * 12).toFixed(0) 
-  const min = Math.round((Math.random() * 60  / 15) * 15)
 
-  // ((((Math.random() + 7.5)/15 | 0) * 15) % 60)
   const output = `  
-    * Thank you for TimePickr *
+    * Thank you for using TimePickr *
     ***************************
-    Here is your chosen time
-    ${hour}:${min} ${response.amOrPm}
+    Here is your generated time
+    ${hour}:${minGen()}${response.amOrPm}
     `;
+
   console.log(output);
 })();
