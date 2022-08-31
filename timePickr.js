530e8a6cfec4e411e240e0dafe3c2c1a52d535bd
Don Stevenson <dstevenson@tribalscale.com>
2022-08-30 21:35:09 -0400

    Refactoring validateAmOrPm

--- timePickr.js
+++ timePickr.js
@@ -1,19 +1,19 @@
-const prompts = require("prompts");
+const prompts = require("prompts")
 
 // helpers
-const validateAmOrPm = (value) => {
-  if (value === "am" || value === "pm") true;
-  else `Error!! Please enter either "am" or "pm" only`;
-};
+const validateAmOrPm = value =>
+  value === "am" || value === "pm"
+    ? true
+    : `Error!! Please enter either "am" or "pm" only`
 
-const hour = (Math.random() * 12).toFixed(0);
+const hour = (Math.random() * 12).toFixed(0)
 
 const minGen = () => {
-  const mins = Math.round((Math.random() * 60) / 15) * 15;
-  return mins === 60 || mins === 0 ? "00" : mins;
-};
+  const mins = Math.round((Math.random() * 60) / 15) * 15
+  return mins === 60 || mins === 0 ? "00" : mins
+}
 
-const onCancel = () => true;
+const onCancel = () => true
 
 // questions
 const questions = [
@@ -29,13 +29,13 @@ A program to help you pick a time
 Choose morning (am) or evening (pm)?
 or hit "escape" to quit
 `,
-    validate: (amOrPm) => validateAmOrPm(amOrPm),
+    validate: amOrPm => validateAmOrPm(amOrPm),
   },
-];
+]
 
 // run program
-(async () => {
-  const response = await prompts(questions, { onCancel });
+;(async () => {
+  const response = await prompts(questions, { onCancel })
 
   const output = `  
   * Thank you for using TimePickr *
@@ -43,11 +43,11 @@ or hit "escape" to quit
   Here is your generated time
 
   ${hour}:${minGen()}${response.amOrPm}
-  `;
+  `
 
-  if (response.amOrPm) console.log(output);
+  if (response.amOrPm) console.log(output)
   else
     console.log(`
   'The program has been aborted. Goodbye! 
-  `);
-})();
+  `)
+})()
