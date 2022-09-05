8463d68c98a20590be9a92a1b127a3c449be7899
Don Stevenson <dstevenson@tribalscale.com>
2022-09-04 22:18:21 -0400

    Update to remove hours > 7 and < 1

--- helpers/hour.js
+++ helpers/hour.js
@@ -1,3 +1,7 @@
-const hour = (Math.random() * 12).toFixed(0)
+const hour = () => {
+  const generatedHour = (Math.random() * 12).toFixed(0)
+  if (generatedHour > 7 || generatedHour < 1) return hour()
+  else return generatedHour
+}
 
 exports.hour = hour
