2e94541b56c021552b979010262c5a44f849a3c0
Don Stevenson <dsteven1@gmail.com>
2022-10-08 00:00:21 -0400

    refactor to one line hour function

--- helpers/hour.js
+++ helpers/hour.js
@@ -1,6 +1,3 @@
-const hour = () => {
-  const generatedHour = (Math.random() * 12).toFixed(0)
-  return generatedHour > 12 || generatedHour < 1 ? hour() : generatedHour
-}
+const hour = () => (Math.random() * 12).toFixed(0)
 
 exports.hour = hour
